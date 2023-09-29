12b3d6df341193dd28b148d6b02267079b1c85a1
MatthewRittmeier <79926449+MatthewRittmeier@users.noreply.github.com>
2023-09-28 20:55:14 -0400

    v1

---
+++ src/components/BottomBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function BottomBar() {
+    return (
+        <>
+            <div className="TopInfo">
+                <div className="VerticalSpacer"/>
+                    Hi, didn't expect to see you there...
+                <div className="VerticalSpacer"/>
+            </div>
+        </>
+    )
+}
+
+export default BottomBar;
